63a72649b2962d99fde49af1c5d42a489b740269
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2022-12-03 22:44:10 +1100

    Add day 1 task

---
+++ third_party/aoc/2022/task1.c
@@ -0,0 +1,93 @@
+// clang-format off
+#include "libc/inttypes.h"
+#include "libc/limits.h"
+#include "libc/fmt/conv.h"
+#include "libc/mem/gc.h"
+#include "libc/runtime/runtime.h"
+#include "libc/str/str.h"
+#include "libc/x/xasprintf.h"
+
+#include "libc/stdio/stdio.h"
+
+#define STRINGIFY(x) #x
+#define LENGTH(x) (sizeof(STRINGIFY(x)) - 1)
+
+char* year = "2022";
+char* inputFileName = "task1.data";
+char* test = "/home/joshua/src/src/cosmo/cosmoaoc/cosmopolitan/third_party/aoc/2022/task1";
+
+void task1()
+{
+    char* comPath = GetProgramExecutableName();
+    dirname(comPath);
+    
+    char* comPathOLocation = strstr(comPath, "/o/");
+    size_t comPathPrefixLength = comPathOLocation - comPath;
+    size_t comPathSuffixLength = strlen(comPath) - comPathPrefixLength - 2;
+    
+    char* inputPath = _gc(xasprintf("%.*s%.*s/%s/%s", comPathPrefixLength, comPath, comPathSuffixLength, comPath + comPathPrefixLength + 2, year, inputFileName));
+    
+    // Based on top example from https://solarianprogrammer.com/2019/04/03/c-programming-read-file-lines-fgets-getline-implement-portable-getline/
+    // as the issues presented are not relevant here
+    FILE *fp = fopen(inputPath, "r");
+    if (fp == NULL)
+    {
+        perror("Unable to open input file");
+        exit(1);
+    }
+    
+    // Cosmo does not have UINT_WIDTH so using a macro to determine it
+    char chunk[LENGTH(UINT_MAX) + 1];
+    
+    uint_fast32_t elfNum = 1;
+    uint_fast32_t currentElfCalories = 0;
+    uint_fast32_t thirdHighestElfCalories = 0;
+    uint_fast32_t secondHighestElfCalories = 0;
+    uint_fast32_t highestElfCalories = 0;
+    
+    while(fgets(chunk, sizeof(chunk), fp) != NULL)
+    {
+        if (chunk[0] == '\n')
+        {
+            elfNum++;
+            // There must be a better method with less branching
+            // Initially I thought  of 
+            if (currentElfCalories > thirdHighestElfCalories)
+            {
+                if (currentElfCalories > secondHighestElfCalories)
+                {
+                    thirdHighestElfCalories = secondHighestElfCalories;
+                    if (currentElfCalories > highestElfCalories)
+                    {
+                        secondHighestElfCalories = highestElfCalories;
+                        highestElfCalories = currentElfCalories;
+                    }
+                    else
+                    {
+                        secondHighestElfCalories = currentElfCalories;
+                    }
+                }
+                else
+                {
+                    thirdHighestElfCalories = currentElfCalories;
+                }
+            }
+            currentElfCalories = 0;
+            continue;
+        }
+        
+        unsigned long input = strtoul(chunk, NULL, 10);
+        if (chunk[0] == '-' || input == 0 || input >= UINT_MAX)
+        {
+            perror("Unable to parse input file");
+            exit(1);
+        }
+        
+        currentElfCalories += input;
+    }
+    
+    printf("Part 1: Highest number of calories:              %i\n", highestElfCalories);
+    printf("Part 2: Sum of three highest number of calories: %i\n", highestElfCalories + secondHighestElfCalories + thirdHighestElfCalories);
+    
+    fclose(fp);
+}
--- third_party/aoc/aoc.c
+++ third_party/aoc/aoc.c
@@ -1,6 +1,10 @@
 // clang-format off
+#include "third_party/aoc/2022/task1.c"
+
 int main()
 {
+	task1();
+
 	return 0;
 }
 
